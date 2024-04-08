143531ae2ab5a00e381d5948cf4666514920e472
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-04-08 21:19:33 +0100

    086-15

---
+++ 04_python/aula_086/exercicio_15/funcoes.py
@@ -0,0 +1,35 @@
+import time
+import os
+
+
+
+def getValor(valor):
+    if(valor >= 0):
+        return 'VALOR POSITIVO!'
+    else:
+        return 'VALOR NEGATIVO!'
+
+
+
+def analisar(tempo):
+    tempo = 0.3
+    limpar()
+    print('A analisar')
+    esperar(tempo)
+    limpar()
+    print('A analisar.')
+    esperar(tempo)
+    limpar()
+    print('A analisar..')
+    esperar(tempo)
+    limpar()
+    print('A analisar...')
+    esperar(tempo)
+    limpar()
+
+#Funcoes Especiais
+def limpar():
+    if(os.name == "nt"): os.system("cls")
+
+def esperar(tempo):
+    time.sleep(tempo)
---
+++ 04_python/aula_086/exercicio_16/main.py
@@ -0,0 +1,13 @@
+import funcoes
+import os 
+import time 
+
+valor = float(input('- Digita um numero: '))
+
+funcoes.limpar()
+
+funcoes.analisar(0.3)
+
+print(funcoes.getValor(valor))
+
+print('\n')
