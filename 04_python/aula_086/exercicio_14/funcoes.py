72e02ee7bb57ed125e9278f3eff56e054dd1e623
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-04-08 21:16:44 +0100

    086-14

---
+++ 04_python/aula_086/exercicio_14/funcoes.py
@@ -0,0 +1,35 @@
+import time
+import os
+
+
+
+def getValor(valor):
+    if(valor >= 10):
+        return 'É MAIOR QUE 10!'
+    else:
+        return 'NÃO É MAIOR QUE 10!'
+
+
+
+def analisar(tempo):
+    tempo = 0.3
+    limpar()
+    print('A analisar')
+    esperar(tempo)
+    limpar()
+    print('A analisar.')
+    esperar(tempo)
+    limpar()
+    print('A analisar..')
+    esperar(tempo)
+    limpar()
+    print('A analisar...')
+    esperar(tempo)
+    limpar()
+
+#Funcoes Especiais
+def limpar():
+    if(os.name == "nt"): os.system("cls")
+
+def esperar(tempo):
+    time.sleep(tempo)
