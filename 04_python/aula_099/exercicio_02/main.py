c05b072e59dcd679bbf79d775dc2e443f33e79f2
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-04-29 21:04:39 +0100

    099

---
+++ 04_python/aula_099/exercicio_02/main.py
@@ -0,0 +1,45 @@
+import funcoes
+
+funcoes.limpa()
+
+
+frutas = ['uva', 'maça', 'morango', 'ananás', 'banana', 'laranja']
+
+print(frutas)
+
+print()
+
+print(f'O primeiro elemento é: {frutas[0]}')
+
+print()
+
+
+
+print(f'Ultimo elemento, positivo: {frutas[5]}\n')
+
+
+print()
+
+
+print(f'Ultimo elemento, negativo: {frutas[len(frutas) - 1]}\n')
+
+
+print()
+
+sublista = frutas[1:4]
+
+print(f'Sublista do 2º ao 4º elemento: {sublista}\n')
+
+
+print()
+
+frutas[1] = 'clementinas'
+
+print(frutas)
+
+
+
+
+
+
+print('\n')
---
+++ 04_python/aula_100/exercicio_01/main.py
@@ -0,0 +1,41 @@
+import funcoes
+
+funcoes.limpa()
+
+
+frutas = ['uva', 'maça', 'morango']
+
+print(frutas)
+
+print()
+
+print('Adicionar ananás\n')
+
+frutas.append('ananás')
+print(frutas)
+
+print()
+
+print('Laranja na segunda posição\n')
+
+frutas.insert(1, 'laranja')
+print(frutas)
+
+print()
+
+print('Remover primeiro elemento\n')
+
+frutas.pop(0)
+
+print(frutas)
+
+print()
+
+print('Remover morango\n')
+
+frutas.remove('morango')
+    
+print(frutas)
+
+
+print('\n')
