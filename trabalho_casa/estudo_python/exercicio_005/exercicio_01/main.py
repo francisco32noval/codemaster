f6874384d9c6333bd558e5d7917431bc2958ce6c
francisco32noval <163427990+francisco32noval@users.noreply.github.com>
2024-05-07 17:12:50 +0100

    tpc

---
+++ trabalho_casa/estudo_python/exercicio_005/exercicio_01/main.py
@@ -0,0 +1,58 @@
+import funcoes
+
+funcoes.limpa()
+
+for i in range(10):
+    print(i + 1)
+
+print()
+
+for i in range(9, -1, -1):
+    print(i + 1)
+
+
+print()
+
+for i in range(101, 112):
+    print(i)
+
+
+
+print()
+
+# valor = -1
+
+# # Loop que continua até que um valor inteiro não negativo seja recebido
+# while valor < 0:
+#     entrada = int(input("Digite um valor inteiro não negativo: "))
+#     if entrada < 0:
+#         print("Por favor, digite um número maior ou igual a zero.")
+#     else:
+#         valor = entrada
+
+# # Loop for para imprimir os números de 0 até o valor
+# for i in range(valor + 1):
+#     print(i)
+
+
+print()
+
+loop = 1
+
+while(loop <= 10):
+    print(f'8 * {loop} = {8 * loop}')
+    loop += 1
+
+
+print()
+
+loop = 1
+
+while(loop <= 10):
+    numero = int(input('- Digita um numero inteiro positivo: '))
+    for i in range()
+    print(f'{numero} * {loop} = {numero * loop}')
+    loop += 1
+
+
+print('\n')
